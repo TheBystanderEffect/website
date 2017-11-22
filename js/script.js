ea9bd5b5392da782e255b1ff155575c21e0e9059
SecoWWW <patrik.scensny@gmail.com>
2017-11-22 15:46:49 +0100

    updated img of prototype, and added polasek img

--- js/script.js
+++ js/script.js
@@ -144,7 +144,7 @@ $(document).ready(function(){
       items: 4
   });
 
-  $("#polasek").css("width","138").css("height","220");
+  $("div #polasek.img-rounded").css("width","138").css("height","220").css("margin-left","111");
 
 
 });
